c2716f0eb8f74fd0a00778fa77e04bb02547d672
Lena Porina <porinalena@gmail.com>
2020-06-08 21:27:49 -0700

    round height and weight values; make changing slider values appear on the screen

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-    
-    @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print("height\(sender.value)")
-    }
-    
-    @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print("weight\(sender.value)")
-    }
+        
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", (sender.value))
+        weightLabel.text = "\(weight)Kg"
+    }
+    
 
 
 }
