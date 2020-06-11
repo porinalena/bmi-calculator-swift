5503529ec038386e060be21e8ba21b232aad78c0
Lena Porina <porinalena@gmail.com>
2020-06-10 20:08:25 -0700

    add bmi calculator

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,13 @@ class ViewController: UIViewController {
         weightLabel.text = "\(weight)Kg"
     }
     
-
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+    
+        print(bmi)
+    }
+    
 }
 
